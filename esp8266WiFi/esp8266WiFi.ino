b88611edd02f34bd78526e28c8f7b984f2020a38
Adriel M. D'nardi <75861319+adrielnardi@users.noreply.github.com>
2021-08-19 17:37:04 -0300

    esp8266WiFi
    ESP8266 Wifi conectando-se a uma rede WiFi e obtendo seu IP local.

---
+++ esp8266WiFi/esp8266WiFi.ino
@@ -0,0 +1,24 @@
+//Biblioteca para conexão do WiFi com o Esp8266
+#include <ESP8266WiFi.h>
+char ssid[] = "Oficial_2.4G";     //Nome da  rede
+char password[] = "XXXXXXX"; //Senha da rede
+
+void setup()
+{
+  //Inicialização da comunicação Serttial e da comunicação Wifi
+  Serial.begin(115200);
+  Serial.println();
+  WiFi.begin(ssid, password);
+
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED) //retorna se houve problema de conexão ou não. 
+  {
+    delay(500); //tempo de meio segundo e vai tentar repetir a conexão
+    Serial.print("."); //se a conexão não ofuncionar e algo tiver errado, vai mostrar “Connecting to WiFi..”.
+  }
+  Serial.println();
+
+  Serial.print("Connected, IP address: "); //Assim que conectar vai mostrar seu Endereço IP Local
+  Serial.println(WiFi.localIP());
+}
+void loop() {}
